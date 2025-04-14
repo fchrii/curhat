7dd94d85de2abd9defa8e52d43daefcd764ec963
fchrii <fachriahsani89@gmail.com>
2025-04-14 20:29:16 +0700

    edit gif

--- app/page.tsx
+++ app/page.tsx
@@ -80,7 +80,7 @@ export default function Home() {
               <div className="flex justify-center">
                 <div className="relative w-full h-[200px] rounded-lg overflow-hidden">
                   <Image
-                    src={response.gif || "/placeholder.svg"}
+                    src={https://tenor.com/view/b-eat-saber-gif-26237883}
                     alt="Motivational GIF"
                     fill
                     className="object-cover"
