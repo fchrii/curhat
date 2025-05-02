662174f43cf66bbcd94999a2df600b180d6b498e
fchrii <fachriahsani89@gmail.com>
2025-05-02 14:39:26 +0700

    update title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Curhat Website',
+  description: 'Created with heart',
 }
 
 export default function RootLayout({
